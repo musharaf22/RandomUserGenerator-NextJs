14dd32e03af8ac4336f5bba47fa1bdbc2809cc1b
Musharaf <105627926+musharaf-fleapo@users.noreply.github.com>
2024-06-29 13:26:29 +0530

    sorting and pagination added to api endpoint

---
+++ src/app/api/user/route.ts
@@ -0,0 +1,58 @@
+import prisma from "@/utils/prismaClient";
+import { NextResponse } from "next/server";
+
+export async function GET(req: Request) {
+  const url = new URL(req.url);
+  const searchParams = new URLSearchParams(url.searchParams);
+  const [page = 1, limit = 10, sortKey, sortValue] = [
+    searchParams.get("page") ?? 1,
+    searchParams.get("limit") ?? 10,
+    searchParams.get("sortKey") ?? null,
+    searchParams.get("sortValue") ?? null,
+  ];
+
+  const allUser = await prisma.user.findMany({
+    orderBy: sortKey
+      ? {
+          [sortKey]: sortValue ?? "asc",
+        }
+      : {
+          createdAt: "desc",
+        },
+    skip: (Number(page) - 1) * Number(limit),
+    take: Number(limit),
+  });
+
+  return NextResponse.json({
+    message: "User fetched successfully",
+    data: allUser,
+  });
+}
+
+export async function POST(req: Request) {
+  console.log({ body: req.body });
+  const resp = await fetch("https://randomuser.me/api");
+  const data = await resp.json();
+  if (!data) throw new Error("unable to generate user information");
+
+  const userData = data.results[0];
+  const userPayload = {
+    firstName: userData.name.first,
+    lastName: userData.name.last,
+    location: `${userData.location.street.name} , ${userData.location.city}`,
+    email: userData.email,
+    age: userData?.registered?.age,
+    images: [
+      userData?.picture.large,
+      userData?.picture.medium,
+      userData?.picture.thumbnail,
+    ],
+  };
+  const createdUser = await prisma.user.create({
+    data: userPayload,
+  });
+  return NextResponse.json({
+    message: "user created successfully",
+    data: createdUser,
+  });
+}
