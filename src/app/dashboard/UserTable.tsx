54950a8d1e375b664eaee5199a8c6e616815a4dd
Musharaf <105627926+musharaf-fleapo@users.noreply.github.com>
2024-06-29 14:03:47 +0530

    integrating api

--- src/app/dashboard/UserTable.tsx
+++ src/app/dashboard/UserTable.tsx
@@ -1,11 +1,25 @@
 "use client";
-import React, { useEffect } from "react";
-import { getRandomUser } from "./serverActions/action";
+import React, { useEffect, useState } from "react";
+import { getAllUser, getRandomUser } from "./serverActions/action";
 
+interface IUser {
+  id: string;
+  email: string;
+  firstName: string;
+  lastName: string;
+  age: number;
+  location: string;
+  images: string[];
+  createdAt: string;
+  updatedAt: string;
+}
 const UserTable = () => {
+  const [user, setUser] = useState<IUser[] | []>([]);
   const getUser = async () => {
-    const user = await getRandomUser();
-    console.log({ user });
+    const user = await getAllUser({ page: 1, limit: 10 });
+    if (user) {
+      setUser(user.data);
+    }
   };
   useEffect(() => {
     getUser();
@@ -15,7 +29,15 @@ const UserTable = () => {
       <table className="w-full ">
         {/* // table heading  */}
         <tr className="text-center p-2">
-          {["Name", "Email", "Phone"].map((e, i: number) => {
+          {[
+            "SL",
+            "Image",
+            "First Name",
+            "Last Name",
+            "Email",
+            "Age",
+            "Location",
+          ].map((e, i: number) => {
             return (
               <th key={i} className="p-2">
                 {e}
@@ -24,11 +46,31 @@ const UserTable = () => {
           })}
         </tr>
         {/* // Table content  */}
-        <tr className="text-center bg-slate-300 p-2 ">
-          <td className="p-2">Alfreds Futterkiste</td>
-          <td className="p-2">Maria Anders</td>
-          <td className="p-2">Germany</td>
-        </tr>
+
+        {user.map((u: IUser, i: number) => {
+          return (
+            <tr
+              className={`text-center ${
+                i + 1 / 2 === 0 ? "bg-slate-500" : "bg-white"
+              } p-2 `}
+              key={u.id}
+            >
+              <td className="p-2">{i + 1}</td>
+              <td className="p-2">
+                <img
+                  src={u.images[0]}
+                  alt=""
+                  className="rounded-full w-[100px] h-[100px]  mx-auto"
+                />
+              </td>
+              <td className="p-2">{u.firstName}</td>
+              <td className="p-2">{u.lastName}</td>
+              <td className="p-2">{u.email}</td>
+              <td className="p-2">{u.age}</td>
+              <td className="p-2">{u.location}</td>
+            </tr>
+          );
+        })}
       </table>
     </div>
   );
--- src/app/dashboard/serverActions/action.ts
+++ src/app/dashboard/serverActions/action.ts
@@ -1,8 +1,22 @@
 "use server";
-
+interface IUserFilter {
+  page: number;
+  limit: number;
+  sortKey?: string;
+  sortValue?: string;
+}
 export const getRandomUser = async () => {
   const resp = await fetch("https://randomuser.me/api");
   const data = await resp.json();
   console.log(data);
   return data;
+};
+
+//Get All user
+export const getAllUser = async (userProps: IUserFilter) => {
+  const searchParam = new URLSearchParams(Object.entries(userProps)).toString();
+  const resp = await fetch(`http://localhost:3000/api/user?${searchParam}`, {
+    method: "GET",
+  });
+  return await resp.json();
 };
